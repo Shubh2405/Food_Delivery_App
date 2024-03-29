1d069fb64dde035585b14978edbf5f68fbda456c
Shubham Sen <160997397+Shubh2405@users.noreply.github.com>
2024-03-29 17:22:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 // app.js
 
-const sequelize = require('./config/database.js'); // Import your Sequelize instance
-const models = require('./models/pricingModel.js'); // Import your Sequelize models
-// const mod = require('./models/itemModel.js');
-// const m = require('./models/pricingModel.js');
+const sequelize = require('./database.js'); // Import your Sequelize instance
+const models = require('./pricingModel.js'); // Import your Sequelize models
+
 
 // Synchronize models with the database
 sequelize.sync({ force: false }) // Set force to true to drop existing tables and re-create them
   .then(() => {
     console.log('Database synchronized successfully');
-    // Start your server or perform other tasks
+    
   })
   .catch((error) => {
     console.error('Database synchronization failed:', error);
@@ -33,7 +32,7 @@ app.listen(PORT, () => {
 });
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json'); // Replace with your generated Swagger specification
+const swaggerDocument = require('./swagger.json'); // Replace with your generated Swagger specification
 
 // Serve Swagger UI at /api-docs endpoint
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
